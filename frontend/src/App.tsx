37ba83246008999a91198ae6982102d8ad22ee40
chirag-droid <chirag.singla.pi@gmail.com>
2025-02-27 12:26:21 +0530

    ui: remove header component from the top

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import store from "@/app/store";
-import Header from "@/features/header";
 import TrendingStocks from "@/features/trending-stocks";
 import { fetchTrendingStocks } from "@/features/trending-stocks/slice";
 import StockGraphs from "@/features/stock-graphs";
@@ -12,8 +11,7 @@ store.dispatch(fetchStocks());
 function App() {
    return (
       <>
-         <Header />
-         <main className="px-4 space-y-2 md:space-y-3 py-3 md:px-6">
+         <main className="px-6 space-y-2 md:space-y-3 py-4 md:px-8">
             <TrendingStocks />
             <StockGraphs />
          </main>
